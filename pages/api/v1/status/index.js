38ac440956f3ba6bbb9ab2be8812188951e7cbc5
Levi Eber Gomes Barbosa <gblevieber@gmail.com>
2024-07-08 21:04:25 -0300

    chore: add `database.js` and `.env.development` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(_request, response) {
-  response.status(200).json({ message: "Olá, mundo!" });
+import database from "../../../../infra/database.js";
+
+async function status(_request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows)
+  response.status(200).json({ message: result });
 }
 
 export default status;
