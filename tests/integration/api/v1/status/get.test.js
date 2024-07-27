edf79dd3d5a75a033006ce8f15232ed6aa173373
Levi Eber Gomes Barbosa <gblevieber@gmail.com>
2024-07-27 19:44:44 -0300

    style: organize test code to be Triple A pattern compatible

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,10 +1,8 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
-  expect(response.status).toBe(200);
-
   const responseBody = await response.json();
 
+  expect(response.status).toBe(200);
   expect(Date.parse(responseBody.updated_at)).not.toBe(NaN);
   expect(responseBody.dependencies.database.version).toBe("16.0");
   expect(responseBody.dependencies.database.max_connections).toBe(100);
