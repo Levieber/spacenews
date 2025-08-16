747a41e71885330746bb70647937c5a8d70a62e8
Levi Eber Gomes Barbosa <gblevieber@gmail.com>
2025-08-16 02:47:24 +0100

    feat: invalidate session cookie on unauthorized error

--- tests/integration/api/v1/sessions/delete.test.js
+++ tests/integration/api/v1/sessions/delete.test.js
@@ -24,6 +24,18 @@ describe("DELETE /api/v1/sessions", () => {
         action: "Verifique se este usuário está logado e tente novamente.",
         status_code: 401,
       });
+
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
+      });
     });
 
     test("With expired session", async () => {
@@ -51,6 +63,18 @@ describe("DELETE /api/v1/sessions", () => {
         message: "Usuário não possui sessão ativa.",
         action: "Verifique se este usuário está logado e tente novamente.",
         status_code: 401,
+      });
+
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
       });
     });
 
@@ -114,6 +138,21 @@ describe("DELETE /api/v1/sessions", () => {
         message: "Usuário não possui sessão ativa.",
         action: "Verifique se este usuário está logado e tente novamente.",
         status_code: 401,
+      });
+
+      const doubleCheckResponseParsedSetCookie = setCookieParser(
+        doubleCheckResponse,
+        {
+          map: true,
+        },
+      );
+
+      expect(doubleCheckResponseParsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: "invalid",
+        maxAge: -1,
+        path: "/",
+        httpOnly: true,
       });
     });
   });
