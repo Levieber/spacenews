975f0ed5de37a597b60bedf7ef0f0b7a111ccc22
Levi Eber Gomes Barbosa <gblevieber@gmail.com>
2025-10-17 01:43:28 +0100

    feat: send activation email after `user` registration

---
+++ infra/migrations/1760659553559_create-user-activation-tokens.js
@@ -0,0 +1,38 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("user_activation_tokens", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    used_at: {
+      type: "timestamptz",
+      notNull: false,
+    },
+    user_id: {
+      type: "uuid",
+      notNull: true,
+    },
+    expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
+};
+
+export const down = false;
