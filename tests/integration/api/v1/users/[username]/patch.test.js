1679581dd0c7696ba910b50e5815889a43359cf9
Levi Eber Gomes Barbosa <gblevieber@gmail.com>
2026-03-23 22:28:44 +0000

    test: assert `email` is updated in database after `PATCH /api/v1/users/[username]`

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -259,6 +259,12 @@ describe("PATCH /api/v1/users/[username]", () => {
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
 
       expect(responseBody.updated_at > responseBody.created_at).toBe(true);
+
+      const userInDatabase = await user.findOneByUsername(
+        activatedUser.username,
+      );
+
+      expect(userInDatabase.email).toBe(expectedEmail);
     });
 
     test("With new `password`", async () => {
