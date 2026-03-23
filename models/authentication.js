634af4a763027bbcaf9dc3cbfef32c3959cd0725
Levi Eber Gomes Barbosa <gblevieber@gmail.com>
2026-03-23 22:01:00 +0000

    refactor: rename `authentication.getAuthenticatedUser` to `authentication.getUser`

--- models/authentication.js
+++ models/authentication.js
@@ -2,7 +2,7 @@ import { NotFoundError, UnauthorizedError } from "@infra/errors.js";
 import user from "@models/user.js";
 import password from "@models/password.js";
 
-async function getAuthenticatedUser(providedEmail, providedPassword) {
+async function getUser(providedEmail, providedPassword) {
   try {
     const storedUser = await findUserByEmail(providedEmail);
 
@@ -42,7 +42,7 @@ async function getAuthenticatedUser(providedEmail, providedPassword) {
 }
 
 const authentication = {
-  getAuthenticatedUser,
+  getUser,
 };
 
 export default authentication;
