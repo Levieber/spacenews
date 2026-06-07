330285201ea1991ebc56c649e506b17744f4157f
Levi Eber Gomes Barbosa <gblevieber@gmail.com>
2026-06-07 14:42:13 +0100

    chore: upgrade dependencies and add paths field to knip configuration

--- knip.js
+++ knip.js
@@ -3,6 +3,11 @@
  */
 const config = {
   ignore: ["infra/migrations/**"],
+  paths: {
+    "@models/*": ["./models/*"],
+    "@infra/*": ["./infra/*"],
+    "@tests/*": ["./tests/*"],
+  },
   ignoreDependencies: [
     "dotenv-expand",
     "@secretlint/secretlint-rule-preset-recommend",
